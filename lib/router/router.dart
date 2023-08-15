d8c6e0e0e02368752d7627ff3799222a3d0a2cb9
CheolheeLee0 <wks0968@gmail.com>
2023-08-15 13:35:56 +0900

    feat: architecture template (models, providers, ui, views)

---
+++ lib/router/router.dart
@@ -0,0 +1,30 @@
+
+// import 'package:remomdeling/detail_screen.dart';
+// import 'package:remomdeling/home_screen.dart';
+// import 'package:remomdeling/model/festival_model.dart';
+// import 'package:remomdeling/onboarding_screen.dart';
+import 'package:go_router/go_router.dart' show GoRoute, GoRouter;
+
+final router = GoRouter(
+  routes: [
+    // GoRoute(
+    //   path: '/',
+    //   builder: (context, state) => const OnboardingScreen(),
+    // ),
+    // GoRoute(
+    //   path: '/home',
+    //   builder: (context, state) =>
+    //       const HomeScreen(title: 'Flutter Demo Home Page'),
+    // ),
+    // GoRoute(
+    //   path: '/detail',
+    //   builder: (context, state) {
+    //     FestivalModel festivalModel =
+    //         FestivalModel.fromJson(state.extra as Map<String, dynamic>);
+    //     return DetailScreen(
+    //       festivalModel: festivalModel,
+    //     );
+    //   },
+    // ),
+  ],
+);
